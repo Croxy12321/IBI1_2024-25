56e1858d04dfe20f1291a15136019bf66b6cb09e
Croxy12321 <2766197709@qq.com>
2025-03-14 11:20:32 +0800

    Update triangle.py

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -1,4 +1,8 @@
+#Print the title of the program
 print("the first ten triangular number:")
+#Loop through numbers 1 to 10
 for n in range(1, 11):
+    #Calculate the triangular number for the current n
     triangle_number = sum(range(1, n + 1))
+    #Print the calculated triangular number
     print(triangle_number)
