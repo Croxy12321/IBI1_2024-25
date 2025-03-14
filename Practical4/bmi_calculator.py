d2da8418fa32c9b6df13344ffeb293b4c50630e6
Croxy12321 <2766197709@qq.com>
2025-03-14 11:11:24 +0800

    Update bmi_calculator.py

--- Practical4/bmi_calculator.py
+++ Practical4/bmi_calculator.py
@@ -1,11 +1,14 @@
+#Prompt user to enter weight and height
 weight = float(input("Enter your weight（kg）："))
 height = float(input("Enter your height（m）："))
+#Calculate BMI
 bmi = weight / (height ** 2)
+#Determine weight category based on BMI
 if bmi < 18.5:
     category = "underweight"
 elif 18.5 <= bmi < 30:
     category = "normal"
 else:
     category = "obese"
-
+#Output the result
 print(f"your BMI is {bmi:.2f}，you are {category}")
