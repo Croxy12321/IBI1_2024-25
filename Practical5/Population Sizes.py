1675e23de0dddfb26ae71364c6c2eef41df3e7bf
Croxy12321 <2766197709@qq.com>
2025-03-18 11:12:14 +0800

    Create Population Sizes.py

---
+++ Practical5/Population Sizes.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+#creat dictionary
+population_data = {"UK": [57.11, 3.13, 1.91, 5.45],"Zhejiang neighbouring provinces": [65.77, 41.88, 45.28, 61.27, 85.15]}
+#sort data
+sorted_uk = sorted(population_data["UK"])
+sorted_zhejiang = sorted(population_data["Zhejiang neighbouring provinces"])
+#print sorted data
+print("Sorted UK populations:", sorted_uk)
+print("Sorted Zhejiang neighbouring provinces populations:", sorted_zhejiang)
+#creat the first pie chart
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1) 
+plt.pie(sorted_uk, labels=['England', 'Scotland', 'Wales', 'Northern Ireland'], autopct='%1.1f%%', startangle=140)
+plt.title('Population Distribution in UK')
+#creat another pie chart
+plt.subplot(1, 2, 2)  
+plt.pie(sorted_zhejiang, labels=['Zhejiang', 'Fujian', 'Jiangxi', 'Anhui', 'Jiangsu'], autopct='%1.1f%%', startangle=140)
+plt.title('Population Distribution in Zhejiang Neighbouring Provinces')
+plt.show()
