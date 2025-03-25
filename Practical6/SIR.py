ccdf9a9bbd036061cb9fd7f2d8fe288b02139d9c
Croxy12321 <2766197709@qq.com>
2025-03-25 10:40:17 +0800

    Update SIR.py

--- Practical6/SIR.py
+++ Practical6/SIR.py
@@ -22,5 +22,34 @@ for t in range(1000):
   IR=I/N
   # Calculate the probability that a susceptible person will be infected
   IP=beta*IR
-  # Randomly select susceptible people to become infected
-  np
+  # Randomly select susceptible people to become infected, NI means new infections
+  NI=np.random.choice(range(2), size=S, p=[1-IP,IP]).sum()
+  # Infected persons were randomly selected to become recovered, NR means new recoveries
+  NR=np.random.choice(range(2), size=I, p=[1-gamma,gamma]).sum()
+  # Update status
+  S=S-NI
+  I=I+NI-NR
+  R=R+NR
+  # Records the S, I, and R values of the current point in time
+  S_list.append(S)
+  I_list.append(I)
+  R_list.append(R)
+  # If all infected people recover, end the cycle early
+  if I==0:
+    break
+# Set the chart size and resolution
+plt.figure(figsize=(6,4) , dpi =150)
+# Plot the change curve of S, I and R
+plt.plot(S_list, label='Susceptible')
+plt.plot(I_list, label='Infected')
+plt.plot(R_list, label='Recovered')
+# Set the horizontal and vertical headings of the chart and the general headings
+plt.xlabel(time)
+plt.ylabel(number of people)
+plt.title(SIR.model)
+plt.legend()
+# Show the chart
+plt.show()
+# Save the chart
+plt.savefig('SIR_model.png', type='png')
+
