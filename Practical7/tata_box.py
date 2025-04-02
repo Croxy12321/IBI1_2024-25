001645757b84023323da967c2a3508322d99f7e5
Croxy12321 <2766197709@qq.com>
2025-04-02 13:38:37 +0800

    update

--- Practical7/tata_box.py
+++ Practical7/tata_box.py
@@ -1,36 +1,25 @@
 import re
-# open the file and read lines
-with open('C:/Users/27661/Downloads/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r',encoding='utf-8') as infile:
-    lines = infile.readlines()
-# initialize variables
-current_gene = None
-current_sequence = []
-tata_genes = []
-# process each line in the file
-for line in lines:
-    line = line.strip()
-    if line.startswith('>'):
-        # if it is a new gene, process the previous one
-        if current_gene is not None and current_sequence:
-            # detect if the sequence contains TATA box
-            sequence = ''.join(current_sequence)
-            if re.search('TATA[AT][AT]', sequence):
-                tata_genes.append((current_gene, sequence))
-        # reset for the new gene
-        current_gene_1 = line[1:].split()[0]  # get gene name
-        current_gene = re.sub(r'_mRNA','',current_gene_1)  # remove '_mRNA' from gene name
-        current_sequence = []
-    else:
-        # add the sequence line to the current gene's sequence
-        current_sequence.append(line)
-# deal with the last gene in the file
-if current_gene is not None and current_sequence:
-    sequence = ''.join(current_sequence)
-    if re.search('TATA[AT][AT]', sequence):
-        tata_genes.append((current_gene, sequence))
-# write the TATA box genes to the Practical7
-output_path = r'C:/Users/27661/Desktop/IBI/IBIP3/IBI1_2024-25/Practical7/tata_genes.fa'
-# write the TATA box genes to the new file
-with open(output_path, 'w') as outfile:
-    for gene, sequence in tata_genes:
-        outfile.write(f'>{gene}\n{sequence}\n')
+# open the file and set output path
+input_file=open('C:/Users/27661/Downloads/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa','r')
+ouput_file=open('C:/Users/27661/Desktop/IBI/IBIP3/IBI1_2024-25/Practical7/tata_genes.fa','w')
+# read lines
+lines=input_file.read()
+# remove unnecessary content
+lines_preliminary_deletion=re.sub(r'_mRNA.+?]','',lines)
+lines_secondary_deletion=re.sub(r' cdna.+?]','',lines_preliminary_deletion)
+# merge lines
+lines_combination=re.sub('\n','',lines_secondary_deletion)
+# separate the lines and generate a list with 'gene name + sequence' as the elements
+lines_separation=re.sub(r'(?=>)','\n',lines_combination).split('\n')
+# define gene
+gene=[]
+# find TATA box and add it to the list
+for line in lines_separation:
+    if re.search(r'TATA[AT]A[AT]',line):
+         gene.append(line) 
+# turn the list into a string
+gene_string='\n'.join(gene)
+# separate gene names from sequences
+lines_splited=re.sub(r'>(.{7})',r'>\1\n',gene_string)
+# write the output file
+ouput_file.write(lines_splited)
