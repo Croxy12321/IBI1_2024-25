df9a4207d49ecd3f5315e33768b1fa86c40eb372
Croxy12321 <2766197709@qq.com>
2025-04-08 10:35:30 +0800

    Update Patient record management.py

--- Practical8/Patient record management.py
+++ Practical8/Patient record management.py
@@ -1,1 +1,10 @@
-
+# create a class of patient's record
+class patients(object):
+    def __init__(self, name, age, admission_date, history):
+        self.name = name
+        self.age = age
+        self.admission_date = admission_date
+        self.history = history
+    # create a funtion to list the patient's record
+    def print_details(self):
+        print(f"{self.name}, {self.age}, {self.admission_date}, {self.history}")
