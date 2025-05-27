b476ee5ab64f54c4c714342278ba62b70c3c7738
Croxy12321 <2766197709@qq.com>
2025-05-27 21:55:30 +0800

    update tata box
    solve the problem of gene name and polish the commitment

--- Practical7/tata_box.py
+++ Practical7/tata_box.py
@@ -1,22 +1,35 @@
 import re
-# open the file and set output path
-input_file=open('C:/Users/27661/Downloads/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa','r')
-ouput_file=open('C:/Users/27661/Desktop/IBI/IBIP3/IBI1_2024-25/Practical7/tata_genes.fa','w')
-# read lines
-lines=input_file.read()
-# merge lines
-lines_combination=re.sub('\n','',lines)
-# separate the lines and generate a list with 'gene name + sequence' as the elements
-lines_separation=re.sub(r'(?=>)','\n',lines_combination).split('\n')
-# define gene
-gene=[]
-# find TATA box and add it to the list
-for line in lines_separation:
-    if re.search(r'TATA[AT]A[AT]',line):
-         gene_name=re.findall(r'gene:(\S+)',line) # extract gene name using regex
-         gene_sequence=re.findall(r'](.+)',line) # extract gene sequence using regex
-         gene.append((gene_name[0],gene_sequence[0])) # add gene name and sequence to the list
-# output the result to a file
-for line in gene:
-    ouput_file.write(line[0]+'\n')
-    ouput_file.write(line[1]+'\n')
+# open the file and set the output path
+input_file = open('C:/Users/27661/Downloads/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r')
+output_file = open('C:/Users/27661/Desktop/IBI/IBIP3/IBI1_2024-25/Practical7/tata_genes.fa', 'w')
+
+# define gene name and sequence variables
+current_gene_name = None
+current_sequence = ""
+
+# read the fasta file line by line
+for line in input_file:
+    line = line.strip()
+    # if the line is a description line
+    if line.startswith(">"):
+        # if the previous sequence exists, check for TATA box
+        if current_gene_name and current_sequence:
+            if re.search(r'TATA[AT]A[AT]', current_sequence):
+                # write to output file in FASTA format
+                output_file.write(f">{current_gene_name}\n{current_sequence}\n")
+        # extract gene name using regex
+        match = re.search(r'gene:(\S+)', line)
+        current_gene_name = match.group(1) if match else None
+        current_sequence = ""
+    else:
+        # append the sequence part (convert to uppercase)
+        current_sequence += line.upper()
+
+# process the last gene in the file
+if current_gene_name and current_sequence:
+    if re.search(r'TATA[AT]A[AT]', current_sequence):
+        output_file.write(f">{current_gene_name}\n{current_sequence}\n")
+
+# close the files
+input_file.close()
+output_file.close()
